6c6f82b68a86e05f0dbac6f72a03e797e9b238ad
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-17 21:03:22 +0200

    feat(expense): send the data from child to parent

--- src/App.js
+++ src/App.js
@@ -25,9 +25,13 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,18 @@ import React from "react"
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const onSaveExpenseDateHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    }
+    props.onAddExpense(expenseData);
+    //console.log(expenseData)
+  }
+
   return <div className="new-expense">
-    <ExpenseForm />
+    <ExpenseForm onSaveExpenseDate={onSaveExpenseDateHandler} />
   </div>
 }
 
