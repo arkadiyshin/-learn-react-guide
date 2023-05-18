040d50a1e890dc24eb71268d05e7862075018de2
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-18 17:11:38 +0200

    feat(expense): convert hard render list of expenses to map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,7 +18,14 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
+        {props.items.map((expense) =>
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        )}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -37,7 +44,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
