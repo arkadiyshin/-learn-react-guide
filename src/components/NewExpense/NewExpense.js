ca58f088fa9a8e3f795e9eb74dd11eab87ff90ee
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-19 16:55:07 +0200

    feat(expense): add buttons to open and close input form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const onSaveExpenseDateHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     }
     props.onAddExpense(expenseData);
-    //console.log(expenseData)
+    setIsEditing(false);
+  }
+
+  const startEditingHandler = (event) => {
+    setIsEditing(true)
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false)
   }
 
   return <div className="new-expense">
-    <ExpenseForm onSaveExpenseDate={onSaveExpenseDateHandler} />
+    {isEditing && <ExpenseForm onSaveExpenseDate={onSaveExpenseDateHandler} onCancel={stopEditingHandler} />}
+    {!isEditing && <button onClick={startEditingHandler} >Add New Expense</button>}
   </div>
 }
 
